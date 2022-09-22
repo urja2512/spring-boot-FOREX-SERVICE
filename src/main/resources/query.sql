de0686dc513e86891423fe0d702b6603d5e9ec40
Manoharsingh Thakur <urja.manoharsingh-thakur@capgemini.com>
2022-09-22 16:51:24 +0530

    commit

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -4,7 +4,7 @@ INSERT INTO role (id, description, name) VALUES (2, 'User role', 'USER');
 
 --Insert in User Table
 INSERT INTO public.users(id, email, password, phone, username)
-VALUES (4, 'admin@email.com', 'admin', 9280675282, 'admin');
+VALUES (1, 'admin@email.com', 'admin', 9280675282, 'admin');
 
 --Insert in user_roles Table
 INSERT INTO public.user_roles(user_id, role_id)
